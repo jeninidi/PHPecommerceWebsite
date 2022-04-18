8deb08bd2b194d90564a23c59aa92dee783da488
Jeni Dimitrova <jenidimitrova@Jenis-MacBook-Air.local>
2022-04-18 14:23:17 +0200

    inital setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Ecommerce website for PHP mandatory assignment</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta charset="UTF-8">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>  
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+</html> 
+
+<nav class="navbar navbar-default navbar-fixed-top"> 
+    <div class="container">
+        <a href="/phpAssignment2/index.php" class="navbar-brand">Ecommerce shop</a>
+
+        <ul class="nav navbar-nav"></ul>
+        
+    </div>
+
+</nav>
