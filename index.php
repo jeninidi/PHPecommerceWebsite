58c38f9783054fe4ffa9e167f834edfb3d0e6616
Jeni Dimitrova <jenidimitrova@Jenis-MacBook-Air.local>
2022-04-18 21:20:22 +0200

    navbar done

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <title>Ecommerce website for PHP mandatory assignment</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/> 
     <link rel="stylesheet" href="css/main.css"/>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
@@ -15,14 +15,41 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 
+<body>
+     
+    <nav class="navbar navbar-default navbar-fixed-top" id="navbar">  
+        <div class="container">
+            <a href="/phpAssignment2/index.php" class="navbar-brand" id="text">Ecommerce shop</a>
+
+            <ul class="nav navbar-nav">
+                <!-- Dropdown menu -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Men fashion
+                        <span class="caret" ></span>
+                    </a>
+                    
+                    <ul class="dropdown-menu" role="menu">
+                         <li> <a href="#">Shirts </a> </li>
+                         <li> <a href="#">Pants </a> </li>
+                         <li> <a href="#">Shoes </a> </li>
+                         <li> <a href="#">Accessories </a> </li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+
+    </nav>
+
+    <!-- Inserting images -->
+    <div id="background-image">
+        <div id="image-1"></div>
+    </div> 
+
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    </p>
+
+</body>
+ 
 </html> 
-
-<nav class="navbar navbar-default navbar-fixed-top"> 
-    <div class="container">
-        <a href="/phpAssignment2/index.php" class="navbar-brand">Ecommerce shop</a>
-
-        <ul class="nav navbar-nav"></ul>
-        
-    </div>
-
-</nav>
